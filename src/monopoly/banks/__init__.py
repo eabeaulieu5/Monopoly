e2aa79805bed60d3ea0b4ddee6664582e9267dea
eabeaulieu5 <eabeaulieu5@users.noreply.github.com>
2026-08-29 07:36:55 -0400

    fix: export bank classes and BankBase in monopoly.banks

--- src/monopoly/banks/__init__.py
+++ src/monopoly/banks/__init__.py
@@ -3,10 +3,24 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-from .cibc import Cibc
-from .desjardins import Desjardins
+from .base import BankBase  # Adjust relative path according to your structure
+from .citibank import Citibank
+from .dbs import Dbs
+from .hsbc import Hsbc
+from .maybank import Maybank
+from .ocbc import Ocbc
+from .standard_chartered import StandardChartered
+from .trust import Trust
+from .uob import Uob
 
 __all__ = [
-    "Cibc",
-    "Desjardins",
+    "BankBase",
+    "Citibank",
+    "Dbs",
+    "Hsbc",
+    "Maybank",
+    "Ocbc",
+    "StandardChartered",
+    "Trust",
+    "Uob",
 ]
